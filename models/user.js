ea12acdc7c51607a89fd9e4c31325c87d7afeb25
Amar Panchal <amar.panchal@gmail.com>
2016-08-11 05:58:39 -0700

    add login route and authenticate class method

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('user', {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -21,9 +21,9 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
-				len: [7,100]
+				len: [7, 100]
 			},
-			set: function (value) {
+			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
 
@@ -41,11 +41,36 @@ module.exports = function(sequelize, DataTypes) {
 				}
 			}
 		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+
+					// search for attributes
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(foundUser) {
+						if (!foundUser || !bcrypt.compareSync(body.password, foundUser.get('password_hash'))) {
+							return reject();
+						}
+						return resolve(foundUser);
+					});
+				}, function(e) {
+					reject();
+
+				});
+			}
+		},
 		instanceMethods: {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
-			} 
+			}
 		}
 	});
+	return user;
 };
